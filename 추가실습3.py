6c310db77bc57b2f0e13d4e8287b047295598696
egoing <egoing@a.com>
2022-07-28 16:09:10 +0900

    py1

---
+++ 추가실습3.py
@@ -0,0 +1,20 @@
+# Q3 Answer template
+
+def solution(left, right):
+    answer=0
+    for i in range(left, right+1):
+        sum =0
+        for j in range(1, i+1):
+            if i%j==0:
+                sum += 1
+        if sum %2==0:
+            answer += i
+        else:
+            answer -= i
+    
+    return answer
+
+left = 13
+right = 17
+c = solution(left, right)
+print(c)
